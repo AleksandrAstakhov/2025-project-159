a0bf6d928cd0e23eb8ac2099929e4d76839eb855
Astakhov Aleksandr <astakhov.am@phystech.edu>
2025-04-16 05:09:11 +0300

    update

--- data/data_api.py
+++ data/data_api.py
@@ -346,9 +346,7 @@ class DataAPI:
         new_size = current_size + self._buffered_groups
 
         self.root[self.LABELS].resize(new_size)
-        self.root[self.LABELS][current_size:new_size] = self._label_buffer[
-            : self._buffered_groups
-        ]
+        self.root[self.LABELS][current_size:new_size] = self._label_buffer[:self._buffered_groups]
 
     def get_sample(
         self, group_id: int
